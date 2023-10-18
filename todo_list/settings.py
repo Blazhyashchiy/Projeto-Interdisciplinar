59339a34877f6e7ad64396185a8ea40c44378692
Brenner <112525878+LuizBrenner@users.noreply.github.com>
2023-10-18 14:19:56 -0300

    Update settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -114,7 +114,7 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
